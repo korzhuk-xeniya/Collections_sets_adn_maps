09b4522b5d425c79a7bcb4a218bb10e6700c771d
Ксения <korzhuk.xeniya@yandex.ru>
2023-08-06 19:23:30 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,22 +18,51 @@ public class Main {
             }
         }
     }
+
     public static void task2() {
         System.out.println("Задание 2");
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6));
+        List<Integer> nums = new ArrayList<>(List.of(1, 10, 2, 3, 4, 4, 5, 5, 6));
+        List<Integer> numbers = new ArrayList<>();
         for (int number = 0; number < nums.size(); number++) {
             if (nums.get(number) % 2 == 0) {
-                System.out.println(nums.get(number));
+                for (int i = 0; i < nums.size(); i++) {
+                    if (!numbers.contains(nums.get(number))) {
+                        numbers.add(nums.get(number));
+                    }
+                }
             }
         }
+        Collections.sort(numbers);
+        System.out.println(numbers);
     }
+
     public static void task3() {
         System.out.println("Задание 3");
-        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        List<String> words = new ArrayList<>();
+        for (int number = 0; number < strings.size(); number++) {
+            for (int word = 0; word < strings.size(); word++) {
+                if (!words.contains(strings.get(number))) {
+                    words.add(strings.get(number));
+                }
+            }
+        }
+        System.out.println(words);
     }
+
     public static void task4() {
         System.out.println("Задание 4");
-        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        Map<String, Integer> numbersMap = new HashMap<>();
+        Integer counter = 1;
+        for (int number = 0; number < strings.size(); number++) {
+            for (int word = 0; word < strings.size(); word++) {
+                if (!numbersMap.containsKey(strings.get(number))) {
+                    numbersMap.put(strings.get(number), counter++);
+                }
+            }
+        }
+        System.out.println(numbersMap);
     }
 }
 
